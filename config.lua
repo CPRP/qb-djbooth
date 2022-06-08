be4ec3e4817f0275e3c525c16277fb88be1ad70e
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2022-06-08 18:14:19 -0400

    added custom locations

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
-print("QB-DJBooth v1.1 by Jimathy")
+print("QB-DJBooth edit by Jimathy")
 
 Config = {
 	Debug = false, -- Set to true to show target locations
@@ -9,6 +9,7 @@ Config = {
 			DefaultVolume = 0.1, -- 0.01 is lowest, 1.0 is max
 			radius = 30, -- The radius of the sound from the booth
 			coords = vector3(120.0, -1281.72, 29.48), -- Where the booth is located
+			playing = false, -- don't touch
 		},
 		[2] = { -- Sisyphus Theater
 			job = "public", -- "public" makes it so anyone can add music.
@@ -17,6 +18,7 @@ Config = {
 			radius = 200,
 			coords = vector3(206.9, 1181.04, 226.51),
 			soundLoc = vector3(212.32, 1155.87, 227.01), -- Add sound origin location if you don't want the music to play from the dj booth
+			playing = false, -- don't touch
 		},
 		[3] = { -- Henhouse (smokeys MLO coords)
 			job = "henhouse",
@@ -24,6 +26,7 @@ Config = {
 			DefaultVolume = 0.1,
 			radius = 30,
 			coords = vector3(-311.35, 6265.18, 32.06),
+			playing = false, -- don't touch
 		},
 		[4] = { -- Tequilala bar (ingame mlo)
 			job = "tequilala",
@@ -31,6 +34,7 @@ Config = {
 			DefaultVolume = 0.1,
 			radius = 30,
 			coords = vector3(-549.68, 282.64, 82.98),
+			playing = false, -- don't touch
 		},
 		[5] = { -- GabzTuners Radio Prop
 			job = "mechanic",
@@ -38,6 +42,7 @@ Config = {
 			DefaultVolume = 0.1,
 			radius = 30,
 			coords = vector3(127.04, -3030.65, 6.80),
+			playing = false, -- don't touch
 			prop = `prop_radio_01` -- Prop to spawn at location, if the location doesn't have one already
 								   -- (can be changed to any prop, coords determine wether its placed correctly)
 		},		
@@ -47,15 +52,29 @@ Config = {
 			DefaultVolume = 0.1,
 			radius = 30,
 			coords = vector3(1595.53, 6453.02, 26.165),
-			prop = `prop_boombox_01`
+			playing = false, -- don't touch
+			prop = `prop_boombox_01` -- Prop to spawn at location, if the location doesn't have one already
+								     -- (can be changed to any prop, coords determine wether its placed correctly)
 		},
-		[7] = { -- LostMC compound next to Casino
-			gang = "lostmc",
+		[7] = { -- Church
+			job = "public",
 			enableBooth = true,
 			DefaultVolume = 0.1,
-			radius = 20,
-			coords = vector3(983.14, -133.17, 79.59),
-			soundLoc = vector3(988.79, -131.62, 78.89), -- Add sound origin location if you don't want the music to play from the dj booth
+			radius = 30,
+			coords = vector3(-1691.27, -274.25, 52.51),
+			playing = false, -- don't touch
+			prop = `prop_radio_01` -- Prop to spawn at location, if the location doesn't have one already
+									-- (can be changed to any prop, coords determine wether its placed correctly)
+		},
+			[8] = { -- D.I.M.E. Car Club
+			job = "public",
+			enableBooth = true,
+			DefaultVolume = 0.1,
+			radius = 60,
+			coords = vector3(955.38, -2390.53, 21.32),
+			playing = false, -- don't touch
+			prop = `prop_speaker_07` -- Prop to spawn at location, if the location doesn't have one already
+									-- (can be changed to any prop, coords determine wether its placed correctly)
 		},
 	},
 }
